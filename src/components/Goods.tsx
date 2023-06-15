5ce0e9b80246a623dd2738b25ea2a18e0c89c460
sttr19 <stastroshko@gmail.com>
2023-06-16 01:16:11 +0300

    feat:add css modules

--- src/components/Goods.tsx
+++ src/components/Goods.tsx
@@ -1,11 +1,12 @@
 import { PuzzleItem } from "../data/model";
 
+
 function Goods({theme,price,previewImg}:PuzzleItem){
 	return (
 		<div className="goods-block">
+			<img src={previewImg} alt="" />
 			<p>{theme}</p>
 			<p>{price}</p>
-			<img src={previewImg} alt="" />
 		</div>
 	);
 }
--- src/containers/GoodList.tsx
+++ src/containers/GoodList.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import { useSelector,useDispatch } from "react-redux";
 import { selectGoods } from "../redux/catalog/catalogSlice";
 import Goods from "../components/Goods";
+import styles from "../components/Goods.module.css"
 
 export function GoodList(){
 	const goods=useSelector(selectGoods);
 	const dispatch=useDispatch();
 	return (
-		<div className="goods-field">
+		<div className={styles["goods-wrapper"]}>
 			{goods.map(item=><Goods {...item}/>)}
 		</div>
 	)
